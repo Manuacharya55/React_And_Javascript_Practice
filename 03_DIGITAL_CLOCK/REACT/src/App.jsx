1fde45aee2821983a1d1b49ecfd9e3b391ecba41
Manu <belveacharya55@gmail.com>
2025-09-11 10:13:44 +0530

    Digital Clock

---
+++ 03_DIGITAL_CLOCK/REACT/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <h1 id="time">{time}</h1>;
+}
+
+export default App;
